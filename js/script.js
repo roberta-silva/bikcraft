999ec9fca56881c7537554fb538cc14d52e28fff
Roberta Silva <robertadasilva.contato@gmail.com>
2025-07-22 20:39:38 -0300

    ativar itens orcamento

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Ativar Links do Menu
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
@@ -9,3 +10,15 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+//Ativar Itens do Orcamento
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
